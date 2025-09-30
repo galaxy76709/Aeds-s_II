3873ac11c4ad5efd092e0afc3ba60eced7b61ec7
galaxy76709 <kayky.gabriel@sga.pucminas.br>
2025-09-30 08:16:24 -0300

    Games.java -> not finished

--- Aed's_II/Verde/Java/Games.java
+++ Aed's_II/Verde/Java/Games.java
@@ -1,6 +1,7 @@
 package Verde.Java;
 public class Games
 {
+// valores (variaveis) do arquivo csv
     public int                          id; 
     public int             estimatedOwners;
     public int                achievements;
@@ -17,6 +18,7 @@ public class Games
     public String [ ]                 tags;
 
     public Games( 
+    // construtor basico para criacao de valores setados
      int id, int estimatedOwners, int achievements,
      int metacriticScore, double price, double userScore, String name, String releaseDate, 
      String [ ] supportedLanguages, String [ ] publishers, String [ ] developers, 
@@ -38,9 +40,13 @@ public class Games
         this.tags                   =             tags;
     }
 
+    //---------------------------||---------------------------\\
+
     
     public void ERRO ( Games game ) 
     {
+
+    //---------------------------||---------------------------\\
 
         String [ ] relaseDate_TMP = game.releaseDate.split("/"); 
         StringBuilder sb  = new StringBuilder( );  
@@ -57,20 +63,56 @@ public class Games
 
         game.releaseDate = sb.toString( ); 
 
+    //---------------------------||---------------------------\\
+
     String estimatedOwners_STR = String.valueOf(game.estimatedOwners);
     StringBuilder sb_2 = new StringBuilder(); 
 
-for ( int i = 0 ; i < estimatedOwners_STR.length(); i++  )
-{
-    if ( Character.isDigit(estimatedOwners_STR.charAt(i)) )
+    for ( int i = 0 ; i < estimatedOwners_STR.length(); i++  )
     {
-        sb_2.append(estimatedOwners_STR.charAt(i));
-    }
+        if ( Character.isDigit(estimatedOwners_STR.charAt(i)) )
+        {
+            sb_2.append(estimatedOwners_STR.charAt(i));
+        }
 
-}
+    }
 
     game.estimatedOwners = Integer.parseInt(sb_2.toString());
 
-    }
+    //---------------------------||---------------------------\\
+
+    String price_STR = String.valueOf( game.price );
+
+        if ( price_STR.equals("Free to Play") || price_STR.equals("0") )
+        {  game.price = 0.0;  }
+
+    //---------------------------||---------------------------\\
+
+
+    String metacriticScore_STR = String.valueOf( game.metacriticScore );
+        
+        if ( metacriticScore_STR == null || metacriticScore_STR.isEmpty( )) 
+        { game.metacriticScore = -1; }
+    
+
+    //---------------------------||---------------------------\\
+
+
+    String userScore_STR = String.valueOf ( game.userScore ); 
+
+        if ( userScore_STR == null || userScore_STR.equals("tbd") || userScore_STR.isEmpty())
+        { game.userScore = -1.0; }
+
+    //---------------------------||---------------------------\\
+
+
+    String achivemente_STR = String.valueOf(game.achievements);
+
+    if ( achivemente_STR.trim( ).equals(" "))
+       { game.achievements = 0; }
+
+    //---------------------------||---------------------------\\
+
+ }
 
 }
